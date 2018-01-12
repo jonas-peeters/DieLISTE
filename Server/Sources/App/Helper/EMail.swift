3cf9907be83e5e45dd552a22573b99ddab92ea37
Jonas Peeters <jonas.peeters@icloud.com>
2018-01-12 21:31:38 +0100

    E-Mail fixed URLSession is not available on Linux -> Switched to Vapos HTTP Client

--- Server/Sources/App/Helper/EMail.swift
+++ Server/Sources/App/Helper/EMail.swift
@@ -1,5 +1,4 @@
 import Vapor
-import Foundation
 
 /// Send an email to the user where he can verify his/her email address
 ///
@@ -7,12 +6,11 @@ import Foundation
 ///   - email: The users email address
 ///   - username: The users username
 ///   - link: The link where the user can verify his/her email
-///   - config: Config in order to get the mail API key
+///   - drop: Droplet in order to get the mail API key and send the request
 /// - Returns: true on success, false when something goes wrong
-func sendEMailVerificationEMail(email: String, username: String, link: String, config: Config) -> Bool {
+func sendEMailVerificationEMail(email: String, username: String, link: String, drop: Droplet) -> Bool {
     let content = "<html lang=en><meta charset=UTF-8><link href='https://fonts.googleapis.com/css?family=Source+Code+Pro'rel=stylesheet><style>*{margin:0;padding:0;outline:0;box-sizing:border-box}body,html{width:100%;height:100%;background-color:#fafafa;color:#333;-webkit-font-smoothing:antialiased}.container{width:90%;padding:32px;margin:0 auto;position:relative;border-radius:10px;top:5%;text-align:left;background-color:#EEE}.title{font-size:40px;opacity:.9;font-weight:100;font-family:'Source Code Pro',monospace;text-shadow:2px 2px rgba(0,0,0,.1);line-height:100px}.message{font-size:20px;opacity:.8;font-family:'Source Code Pro',monospace;text-shadow:1px 1px rgba(0,0,0,.2);line-height:25px}a.button{font-size:20px;opacity:.8;font-family:'Source Code Pro',monospace;text-shadow:1px 1px rgba(0,0,0,.2);line-height:25px;border-radius:10px;border-width:2px;border-style:solid;border-color:#FFF;background-color:#54C5EC;color:#FFF;padding:5px;margin:40px;text-decoration:none}</style><div class=container><h1 class=title>Willkommen </h1><p class=message>Hallo \(username),</p><br><p class=message>vielen Dank für Deine Anmeldung bei DieLISTE!</p><br><p class=message>Klicke auf den folgenden Button, um deine E-Mail zu verifizieren und alle Funktionen nutzen zu können:</p><br><a class=button href=\(link)>E-Mail Verfizieren</a><br><br><p class=message>Viel Spaß beim Erstellen deiner ersten Liste!</p><br><p class=message>Dein DieLISTE Team!</div>"
-    
-    return sendEMail(to: email, content: content, config: config)
+    return sendEMail(to: email, content: content, drop: drop)
 }
 
 func sendForgotPasswordEMail(email: String, username: String, link: String, config: Config) -> Bool {
@@ -24,33 +22,20 @@ func sendForgotPasswordEMail(email: String, username: String, link: String, conf
 /// - Parameters:
 ///   - email: Target email address
 ///   - content: HTML site as a string. This is what the user will see
-///   - config: Config in order to get the mail API key
+///   - drop: Droplet in order to get the mail API key and send the request
 /// - Returns: true on success, false when something goes wrong
-func sendEMail(to email: String, content: String, config: Config) -> Bool {
-    var success: Bool? = nil
-    let url = URL(string: "https://api.sendgrid.com/v3/mail/send")!
-    var request = URLRequest(url: url)
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    let apiKey = config["email","api_key"]!.string!
-    request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-    request.httpMethod = "POST"
-    
-    let body = "{\"personalizations\": [{\"to\": [{\"email\": \"\(email)\"}],\"subject\": \"E-Mail Verification\"}],\"from\": {\"email\": \"no_reply@die-liste.herokuapp.com\", \"name\":\"DieLISTE\"},\"content\": [{\"type\": \"text/html\",\"value\": \"\(content)\"}]}";
-    
-    request.httpBody = body.data(using: .utf8)
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        guard let _ = data, error == nil else {
-            print("While sending an EMail the folowing error occured: \(error.debugDescription)")
-            success = false
-            return
+func sendEMail(to email: String, content: String, drop: Droplet) -> Bool {
+    if let apiKey = drop.config["email","api_key"]?.string! {
+        let uri = "https://api.sendgrid.com/v3/mail/send"
+        let body = "{\"personalizations\": [{\"to\": [{\"email\": \"\(email)\"}],\"subject\": \"Registrierung DieLISTE\"}],\"from\": {\"email\": \"no_reply@die-liste.herokuapp.com\", \"name\":\"DieLISTE\"},\"content\": [{\"type\": \"text/html\",\"value\": \"\(content)\"}]}";
+        do {
+            let _ = try drop.client.post(uri, query: [:], ["Content-Type" : "application/json", "Authorization" : "Bearer \(apiKey)"], body)
+        } catch {
+            print("Could not send email")
+            return false
         }
-        
-        //let responseString = String(data: data, encoding: .utf8)  //Only for debugging
-        success = true
+        return true
     }
-    task.resume()
-    
-    // Waiting for the task to complete
-    while success == nil {}
-    return success!
+    print("Could not get sendgrid api key")
+    return false
 }
