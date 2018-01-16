4ec9da588359b934f121f6db0199b5b1f533b2a3
lenahellam <hella.megge@gmail.com>
2018-01-16 15:26:22 +0100

    CreateUser mit Layout verknüpft.

--- Application/Login.pas
+++ Application/Login.pas
@@ -5,7 +5,7 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
-  FMX.Edit, FMX.Controls.Presentation, FMX.Layouts, UMain;
+  FMX.Edit, FMX.Controls.Presentation, FMX.Layouts, UMain, ServerAPI;
 
 type
   TForm5 = class(TForm)
@@ -22,6 +22,8 @@ type
     BtnRegistrieren: TButton;
     TitleLabel: TLabel;
     procedure BtnLosClick(Sender: TObject);
+    procedure FormCreate(Sender:TObject);
+    procedure BtnRegistrierenClick(Sender: TObject);
   private
     { Private-Deklarationen }
   public
@@ -30,10 +32,26 @@ type
 
 var
   Form5: TForm5;
+  serverAPI: TServerAPI;
 
 implementation
 
 {$R *.fmx}
+
+procedure TForm5.BtnRegistrierenClick(Sender: TObject);
+var email, name, password: string;
+begin
+   email:= EdtEmail.Text;
+   name:= EdtBenutzername2.Text;
+   password:= EdtPW2.Text;
+   TServerAPI.createUser(email,name, password);
+end;
+
+procedure TForm5.FormCreate(Sender:TObject);
+begin
+  serverAPI := TServerAPI.create();
+end;
+
 
 procedure TForm5.BtnLosClick(Sender: TObject);
 var
